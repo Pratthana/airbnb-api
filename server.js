e686137c097fab9b6027462207dc0fa8547de302
pratthana <pratthana07052@gmail.com>
2020-03-13 18:01:48 +0700

    install cors

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const Place = require('./models/place')
+const cors = require('cors');
+app.use(cors());
 
 const uri = 'mongodb://admin:admin@cluster0-shard-00-00-gnhmf.gcp.mongodb.net:27017,cluster0-shard-00-01-gnhmf.gcp.mongodb.net:27017,cluster0-shard-00-02-gnhmf.gcp.mongodb.net:27017/sample_airbnb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
